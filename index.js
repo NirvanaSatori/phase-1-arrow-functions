ecb1acd53564d343173590a591bd27e9ab1311ea
MORRIS <morris24kariuki@gmail.com>
2022-11-25 19:59:53 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function divide(x,y){
+    x=2000;
+    y=100;
+    return x/y;
+
+}
+//arrow function for squaring
+const square = x => x**2;
+
+//arrow function adds two num
+const add = (x ,y)=> {
+    x=1,y=6
+    return x+y;
+}
